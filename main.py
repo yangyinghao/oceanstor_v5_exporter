ae10b9a020c2795f60d25b6676f63e47c6fa4eed
杨英浩 <yangyinghao@zqyyh.com>
2022-10-03 14:02:50 +0800

    完成登录、登出、获取系统基础信息

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import time
-import random
-from prometheus_client import start_http_server, Gauge
+from datetime import datetime
+import json
 import logging
+import requests
+import http.cookiejar as cj
 
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 logging.basicConfig(level=logging.DEBUG,
                     format='{"asctime":"%(asctime)s","levelname":"%(levelname)s","message":"%(message)s"}',
                     datefmt='%Y-%m-%d %H:%M')  # 设置日志级别
@@ -12,39 +16,90 @@ SleepTime = 10
 # 启动端口
 Port = 8000
 # 存储地址
-Store_Host = "10.1.1.1"
+Store_Host = "192.168.32.99"
 # 存储端口
-Store_Port = 8088
+Store_Port = "8088"
 # 用户名
-Store_UserName = "admin"
-Store_Password = "admin"
-
-Storage_login_status = Gauge('Storage_login_status', 'Description of gauge', ['Store_Host'])
+Store_UserName = "monitor"
+Store_Password = "1qazXSW@3edc"
 
 
 class Store_info:
-    def __int__(self):
-        self.login_status()
 
-    # 登录状态检测
-    def login_status(self):
-        logging.DEBUG("登录状态检测")
-        Storage_login_status.labels(Store_Host=Store_Host).set(1)
+    def __init__(self):
+        self.Store_Host = Store_Host
+        self.Store_Port = Store_Port
+        self.Store_UserName = Store_UserName
+        self.Store_Password = Store_Password
+        self.deviceId = None
+        self.iBaseToken = None
+        self.login_status = 0
+        self.headers = {'Content-Type': 'application/json'}
+        self.r = requests.session()
+        self.r.cookies = cj.LWPCookieJar()
+        self.login()
+        self.system()
+        self.logout()
+
+    def login(self):
+        logging.info("存储地址：" + self.Store_Host)
+        logging.info("存储端口：" + self.Store_Port)
+        logging.info("存储用户：" + self.Store_UserName)
+        url = "https://" + self.Store_Host + ":" + self.Store_Port + "/deviceManager/rest/xxxxx/sessions"
+        logging.debug("登录URL：" + url)
+        data = {"username": self.Store_UserName, "password": self.Store_Password, "scope": "0"}
+        try:
+            response = self.r.post(url, headers=self.headers, verify=False, data=json.dumps(data))
+            logging.debug("登录返回：" + response.text)
+            response = json.loads(response.text)
+            if response["data"]["accountstate"] == 1:
+                logging.info("登录成功")
+                self.deviceId = response["data"]["deviceid"]
+                self.iBaseToken = response["data"]["iBaseToken"]
+                self.headers = {'Content-Type': 'application/json', "iBaseToken": self.iBaseToken}
+                self.login_status = 1
+                logging.info("设备ID：" + response["data"]["deviceid"])
+                logging.info("登录用户：" + response["data"]["username"])
+                logging.info("上次登录时间：" + str(datetime.fromtimestamp(response["data"]["lastlogintime"])))
+                logging.info("上次登录IP：" + response["data"]["lastloginip"])
+            else:
+                logging.error("登录失败")
+
+        except Exception:
+            logging.error("系统异常")
+
+    def system(self):
+        logging.info("获取系统基础信息")
+        url = "https://" + self.Store_Host + ":" + self.Store_Port + "/deviceManager/rest/" + self.deviceId + "/system/"
+        logging.debug("调用URL：" + url)
+        try:
+            response = self.r.get(url, headers=self.headers, verify=False)
+            info = json.loads(response.text)['data']
+            logging.debug(info)
+        except Exception:
+            logging.error("系统异常")
+
+    def logout(self):
+        url = "https://" + self.Store_Host + ":" + self.Store_Port + "/deviceManager/rest/" + self.deviceId + "/sessions"
+        logging.debug("调用URL：" + url)
+        try:
+            self.r.delete(url, headers=self.headers, verify=False)
+            logging.info("登出成功")
+        except Exception:
+            logging.error("系统异常")
 
 
 if __name__ == '__main__':
-    logging.info("启动服务")
-    logging.info("服务端口：" + str(Port))
-    logging.info("数据采集间隔：" + str(SleepTime) + "秒")
-    logging.info("存储地址：" + Store_Host)
-    logging.info("存储端口：" + str(Store_Port))
-    logging.info("存储用户：" + Store_UserName)
-    try:
-        start_http_server(Port)
-        logging.info("服务启动成功")
-        while True:
-            time.sleep(SleepTime)
-            Storage_login_status.labels(Store_Host=Store_Host).set(1)  # 本机IP传入labels，CPU使用率传入value
-    except Exception as e:
-        logging.error("服务异常")
-        logging.error(e)
+    # logging.info("启动服务")
+    # logging.info("数据采集间隔：" + str(SleepTime) + "秒")
+    # logging.info("存储地址：" + Store_Host)
+    # logging.info("存储端口：" + str(Store_Port))
+    # logging.info("存储用户：" + Store_UserName)
+    # try:
+    #     logging.info("服务启动成功")
+    #     while True:
+    #         time.sleep(SleepTime)
+    # except Exception as e:
+    #     logging.error("服务异常")
+    #     logging.error(e)
+    Store_info()
